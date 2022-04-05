7e5f4a80a524e50c628e8c7459422c83af5c35b1
Dye <anthony.dye@ies-co.com>
2022-04-05 14:28:31 -0500

    Created Base Django Project + Created Both Apps

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'super_types',
+    'supers'
 ]
 
 MIDDLEWARE = [
@@ -108,3 +111,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
