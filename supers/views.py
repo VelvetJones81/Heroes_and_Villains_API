a8974cdb2c57dd75fad78b9438e348823d4752a9
Dye <anthony.dye@ies-co.com>
2022-04-06 10:43:18 -0500

    Created Super Serializer Class and File + Updated Projects URLS + Created First View Function

--- supers/views.py
+++ supers/views.py
@@ -8,4 +8,18 @@ from supers import serializers
 
 # Create your views here.
 
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
+
+    
+
 
